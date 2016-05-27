a9e8901ed700f2dd1be7f59dc0d9e3879b894f9c
qonop <qonop.try@gmail.com>
2016-05-27 22:24:23 +0800

    饲料和药品二维码

--- Application/Superadmin/Controller/SafeController.class.php
+++ Application/Superadmin/Controller/SafeController.class.php
@@ -81,4 +81,51 @@ class SafeController extends Controller{
         $this->ajaxReturn($data);
     }
 
+    public function feedQr(){
+        if(IS_GET){
+            
+            $name = I('get.name',null);
+            $time = date('Y-m-d');
+            $data = "扫描结果\n\n 产品名称:".$name."\n 时  间:".$time;
+            //$data = "123486dfhuksfuihkishdiauhlg;iahkyuid hsikhjusdhfsdkhfkjah";
+            //dump($data);exit();
+            $this->qr($data);
+        }else{
+            $this->error('生成二维码出错，请重试');
+        }
+        
+    }
+
+    public function medicineQr(){
+        if(IS_GET){
+            //dump($_GET);exit();
+            $name = I('get.name',null);
+            $use = I('get.use',null);
+            $time = date('Y-m-d');
+            $data = "扫描结果\n\n 产品名称:".$name."\n 作  用:".$use."\n 时  间:".$time;
+            //$data = "123486dfhuksfuihkishdiauhlg;iahkyuid hsikhjusdhfsdkhfkjah";
+            //dump($data);exit();
+            $this->qr($data);
+        }else{
+            $this->error('生成二维码出错，请重试');
+        }
+        
+    }
+
+    private function qr($data)
+      {
+            vendor("phpqrcode.phpqrcode");
+            // 纠错级别：L、M、Q、H
+            $level = 'L';
+            // 点的大小：1到10,用于手机端4就可以了
+            $size = 4;
+            // 下面注释了把二维码图片保存到本地的代码,如果要保存图片,用$fileName替换第二个参数false
+            //$path = "images/";
+            // 生成的文件名
+            //$fileName = $path.$size.'.png';
+            $QRcode = new \QRcode();
+            $QRcode::png($data, false, $level, $size);
+
+      }
+
 }
