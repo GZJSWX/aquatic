a267197cc0480048b86cdb35050cdc888cd074de
ronyao <ronyao.try@gmail.com>
2016-07-08 01:24:08 +0800

    基础管理员注册

--- Application/Baseadmin/Controller/UserController.class.php
+++ Application/Baseadmin/Controller/UserController.class.php
@@ -28,12 +28,30 @@ class UserController extends Controller{
         $this->assign('member_data',$data);
         $this->display();
     }
+//    public function adminRegister(){
+//
+//        $data = D('Member')->adminRegister();
+//        redirect(U("Baseadmin/Index/index"));
+//    }
     public function adminRegister(){
-        
-        $data = D('Member')->adminRegister();
-        redirect(U("Baseadmin/Index/index"));
-    }
+        //$params = I("post.");
 
+        $data = D('Member')->adminRegister();
+        //dump($data);exit();
+        if($data==1){
+            $this->error("已存在此用户名，请选择其他用户名");
+        }elseif($data==2){
+            $this->error("两次密码不同，请重新填写");
+        }elseif($data==4){
+            $this->error("请选择权限");
+        }elseif($data==5){
+            $this->error("注册失败，请重新尝试！");
+        }
+        else{
+            $this->success("注册成功",U("Baseadmin/Index/index"));
+        }
+
+    }
     
     public function getChoose(){
 
